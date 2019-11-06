090bde4303f1d7d6bf2c223cd12d3d28f7f70c2c
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-06 22:12:30 +0100

    make reorder condition to error first, return last, fix some PSR-12 code style formation

--- lib/Auth/Source/scoutnetauth.php
+++ lib/Auth/Source/scoutnetauth.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class sspmod_scoutnetmodule_Auth_Source_scoutnetauth
+ */
 class sspmod_scoutnetmodule_Auth_Source_scoutnetauth extends sspmod_core_Auth_UserPassBase
 {
     /**
@@ -11,111 +14,108 @@ class sspmod_scoutnetmodule_Auth_Source_scoutnetauth extends sspmod_core_Auth_Us
      */
     protected function login(string $username, string $password): array
     {
-
         // AUTH MOT SCOUTNET
         $scoutnetHostname = getenv('SCOUTNET_HOSTNAME');
-        $authUrl = 'https://' . $scoutnetHostname . '/api/authenticate';
-        $postdata = http_build_query(
-            array(
+        $authUrl = "https://{$scoutnetHostname}/api/authenticate";
+        $postData = http_build_query(
+            [
                 'username' => $username,
                 'password' => $password
-            )
+            ]
         );
-        $opts = array(
+        $opts = [
             'http' =>
-            array(
-                'method'  => 'POST',
-                'header'  => 'Content-type: application/x-www-form-urlencoded',
-                'content' => $postdata
-            )
-        );
-        $context  = stream_context_create($opts);
+                [
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => $postData
+                ]
+        ];
+        $context = stream_context_create($opts);
         $authResult = file_get_contents($authUrl, false, $context);
-        if ($authResult <> '') {
-            $authResultObj = json_decode($authResult);
-            if (isset($authResultObj->member->member_no)) {
-                /* Inloggningen lyckades */
 
-                $firstFormatted = self::formatNameForEmail($authResultObj->member->first_name);
-                $lastFormatted = self::formatNameForEmail($authResultObj->member->last_name);
-                $firstlast = $firstFormatted . '.' . $lastFormatted;
-
-                //GET ADDITIONAL ATTRIBUTES FROM USER PROFILE
-                $profileUrl = 'https://' . $scoutnetHostname . '/api/get/profile';
-
-                $options = array(
-                    'http' => array(
-                        'method' => "POST",
-                        'header' => "Authorization: Bearer " . $authResultObj->token . "\r\n"
-                    )
-                );
-
-                $context = stream_context_create($options);
-                $memberResult = file_get_contents($profileUrl, false, $context);
-                $memberResultObj = json_decode($memberResult);
-
-                $group_names = [];
-                $group_nos = [];
-                $group_ids = [];
-                foreach ($memberResultObj->memberships as $memberships) {
-                    foreach ($memberships as $groupkey => $group) {
-                        $group_names[] = $group->group->name;
-                        $group_nos[] = $group->group->group_no;
-                        $group_ids[] = $groupkey;
-                    }
-                }
-
-                //GET USER ROLES
-                $roleUrl = 'https://' . $scoutnetHostname . '/api/get/user_roles';
-
-                $options = array(
-                    'http' => array(
-                        'method' => "POST",
-                        'header' => "Authorization: Bearer " . $authResultObj->token . "\r\n"
-                    )
-                );
-
-                $context = stream_context_create($options);
-                $rolesResult = file_get_contents($roleUrl, false, $context);
-                $rolesResultObj = json_decode($rolesResult);
-
-                // Calculate age (above or under 15?)
-                $bday = new DateTime($memberResultObj->dob);
-                $today = new DateTime('00:00:00');
-                $diff = $today->diff($bday);
-                $age = $diff->y;
-                if ($age < 15) $above_15 = 0;
-                else $above_15 = 1;
-
-                $attributes = array(
-                    'uid' => array($authResultObj->member->member_no),
-                    'email' => array($authResultObj->member->email),
-                    'firstname' => array($authResultObj->member->first_name),
-                    'lastname' => array($authResultObj->member->last_name),
-                    'firstlast' => array($firstlast),
-                    'displayName' => array($authResultObj->member->first_name . ' ' . $authResultObj->member->last_name),
-                    'dob' => array($memberResultObj->dob),
-                    'group_name' => $group_names,
-                    'group_no' => $group_nos,
-                    'group_id' => $group_ids,
-                    'above_15' => array($above_15),
-                    'roles' => array($rolesResult),
-                );
-
-                /* Return the attributes. */
-                return $attributes;
-            } elseif (isset($authResultObj->err)) {
-                /* inloggningen misslyckades */
-                SimpleSAML_Logger::warning('ScoutnetAuth: Felaktigt användarnamn eller lösenord för ' . var_export($username, TRUE) . '.');
-                throw new SimpleSAML_Error_Error('WRONGUSERPASS');
-            } else {
-                throw new SimpleSAML_Error_Error('WRONGUSERPASS');
-            }
-        } else {
+        if (!$authResult) {
             /* uppkopplingen misslyckades */
             SimpleSAML_Logger::warning('ScoutnetAuth: Kunde inte koppla upp mot SCOUTNET. Vänligen försök senare.');
             throw new SimpleSAML_Error_Error('WRONGUSERPASS');
         }
+
+        $authResultObj = json_decode($authResult, true, 512, JSON_THROW_ON_ERROR);
+        if (empty($authResultObj->member->member_no)) {
+            /* inloggningen misslyckades */
+            if (empty($authResultObj->err)) {
+                SimpleSAML_Logger::warning('ScoutnetAuth: Inget medlemsnummer för ' . var_export($username, TRUE) . '.');
+            } else {
+                SimpleSAML_Logger::warning('ScoutnetAuth: Felaktigt användarnamn eller lösenord för ' . var_export($username, TRUE) . '.');
+            }
+            throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+        }
+
+        /* Inloggningen lyckades */
+
+        $firstFormatted = self::formatNameForEmail($authResultObj->member->first_name);
+        $lastFormatted = self::formatNameForEmail($authResultObj->member->last_name);
+        $firstlast = $firstFormatted . '.' . $lastFormatted;
+
+        //GET ADDITIONAL ATTRIBUTES FROM USER PROFILE
+        $profileUrl = "https://{$scoutnetHostname}/api/get/profile";
+
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Authorization: Bearer {$authResultObj->token}\r\n"
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $memberResult = file_get_contents($profileUrl, false, $context);
+        $memberResultObj = json_decode($memberResult, true, 512, JSON_THROW_ON_ERROR);
+
+        $groupNames = [];
+        $groupNos = [];
+        $groupIds = [];
+        foreach ($memberResultObj->memberships as $memberships) {
+            foreach ($memberships as $groupKey => $group) {
+                $groupNames[] = $group->group->name;
+                $groupNos[] = $group->group->group_no;
+                $groupIds[] = $groupKey;
+            }
+        }
+
+        //GET USER ROLES
+        $roleUrl = "https://{$scoutnetHostname}/api/get/user_roles";
+
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Authorization: Bearer {$authResultObj->token}\r\n"
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $rolesResult = file_get_contents($roleUrl, false, $context);
+
+        // Calculate age (above or under 15?)
+        $birthday = new DateTime($memberResultObj->dob);
+        $today = new DateTime('00:00:00');
+        $age = $today->diff($birthday)->y;
+        $above_15 = (int) ($age >= 15);
+
+        /* Return the attributes. */
+        return [
+            'uid' => [$authResultObj->member->member_no],
+            'email' => [$authResultObj->member->email],
+            'firstname' => [$authResultObj->member->first_name],
+            'lastname' => [$authResultObj->member->last_name],
+            'firstlast' => [$firstlast],
+            'displayName' => [$authResultObj->member->first_name . ' ' . $authResultObj->member->last_name],
+            'dob' => [$memberResultObj->dob],
+            'group_name' => $groupNames,
+            'group_no' => $groupNos,
+            'group_id' => $groupIds,
+            'above_15' => [$above_15],
+            'roles' => [$rolesResult],
+        ];
     }
 
     /**
@@ -130,8 +130,7 @@ class sspmod_scoutnetmodule_Auth_Source_scoutnetauth extends sspmod_core_Auth_Us
         $name = strtr($name, ['.-' => '-', '-.' => '-']); // Replace -. and -. with -
         $name = self::removeDiacritics($name); // Replace special characters with normal ones.
         $name = preg_replace('/[^0-9a-z.\-]/i', '', $name); // Remove remaining non-letter and non-number characters.
-        $name = strtolower($name);
-        return $name;
+        return strtolower($name);
     }
 
     /**
